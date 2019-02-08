20c1e7949f2c9d36a1151335fb1e692b74d4180d
mahera <mahera5397@gmail.com>
2019-02-08 02:09:25 +0200

    light

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use imagefmt::ColFmt;
 use imagefmt::Error;
 use imagefmt::ColType;
+use std::cmp::Ordering;
 
 #[derive(Copy, Clone)]
 pub struct TGAColor{
@@ -97,137 +98,62 @@ impl TGAImage{
     }
 
     pub fn fill_triangle(&mut self, color: &TGAColor, final_coord: &[Point]) {
-        if final_coord.len()!=3 {return;}
-        if final_coord[0].x==final_coord[1].x&&final_coord[1].x==final_coord[2].x
-        || final_coord[0].y==final_coord[1].y&&final_coord[1].y==final_coord[2].y{ return; }
-
-        println!("filling triangle {:?}",&final_coord);
-        let mut two_min_y = false;
-        let mut y_apex = None;
-        let mut bottom_point = None;
-        for dot in final_coord.iter() {
-            if y_apex == None {
-                y_apex = Some(dot);
-                bottom_point = Some(dot);
+        let mut first_dot = None;
+        let mut last_dot = None;
+        for point in final_coord{
+            if first_dot == None && last_dot==None {
+                first_dot = Some(point);
+                last_dot = Some(point);
                 continue
             }
-            if dot.y <= y_apex.unwrap().y {
-                if dot.y == y_apex.unwrap().y { two_min_y = true; } else {
-                    y_apex = Some(dot);
-                    two_min_y = false;
-                }
-            }
-            if dot.y > bottom_point.unwrap().y {
-                bottom_point = Some(dot);
-            }
+            if first_dot.unwrap().y>point.y{first_dot=Some(point)}
+            if last_dot.unwrap().y<point.y{last_dot=Some(point)}
         }
-        //flip points if two apexes
-        let (apex_point,bottom_dot)=if two_min_y{
-            (bottom_point.unwrap(),y_apex.unwrap().y)
-        }
-        else {
-            (y_apex.unwrap(),bottom_point.unwrap().y)
-        };
-        let mut left_dot = None;
+        let first_dot=first_dot.unwrap();
+        let last_dot=last_dot.unwrap();
+        let mut middle_dot = None;
         for dot in final_coord.iter() {
-            if dot == apex_point { continue }
-            if left_dot == None {
-                left_dot = Some(dot);
-                continue
-            }
-            if dot.x < left_dot.unwrap().x {
-                left_dot = Some(dot);
+            if dot != first_dot && dot != last_dot { middle_dot = Some(dot) }
+        };
+        //if no middle dot then triangle cant exist
+        if middle_dot==None{return;}
+        let middle_dot=middle_dot.unwrap();
+
+        let mut y_axis_middle=Point::new(first_dot.x,middle_dot.y);
+        let y_axis_last=Point::new(first_dot.x,last_dot.y);
+
+        let mut tg_migle = line_length(&y_axis_middle, middle_dot) / line_length(first_dot, &y_axis_middle);
+        if tg_migle.is_infinite(){tg_migle=0.0}
+        let mut tg_last = line_length(&y_axis_last, last_dot) / line_length(first_dot, &y_axis_last);
+        if tg_last.is_infinite(){tg_last=0.0}
+        if middle_dot.x<first_dot.x{tg_migle=tg_migle*(-1.0)}
+        if last_dot.x<first_dot.x{tg_last=tg_last*(-1.0)}
+
+        let (mut on_mid_border,mut on_last_border)=
+            (first_dot.x as f64-tg_migle,first_dot.x as f64-tg_last);
+        for dy in first_dot.y..middle_dot.y+1 {
+            on_mid_border = on_mid_border + tg_migle;
+            on_last_border = on_last_border + tg_last;
+
+            let (start, end) = if on_mid_border > on_last_border { (on_last_border as usize, on_mid_border as usize) } else { (on_mid_border as usize, on_last_border as usize) };
+            for x_coord in start..end+1 {
+                self.set_pixel_unchecked(x_coord, dy, &color);
             }
         }
-        let left_dot = left_dot.unwrap();
-        let mut right_dot = None;
-        for dot in final_coord.iter() {
-            if dot != apex_point && dot != left_dot { right_dot = Some(dot) }
-        };
-        //TODO
-        if right_dot==None{return;}
-        let right_dot = right_dot.unwrap();
-        let (left_dot,right_dot)=if two_min_y{
-            (right_dot,left_dot)
-        }else {
-            (left_dot,right_dot)
-        };
-        let bottom_dot_right = Point::new(apex_point.x, right_dot.y);
-        let mut tg_right = line_length(&bottom_dot_right, right_dot) / line_length(apex_point, &bottom_dot_right);
-        let bottom_dot_left = Point::new(apex_point.x, left_dot.y);
-        let mut tg_left = line_length(&bottom_dot_left, left_dot) / line_length(apex_point, &bottom_dot_left);
-        if tg_right.is_infinite(){tg_right=0.0}
-        if tg_left.is_infinite(){tg_left=0.0}
-        let mut counter = 0;
-        for dot in final_coord.iter() {
-            if dot.x < apex_point.x { counter += 1; }
-        }
-        let clousure;
-        match counter {
-            0 => clousure = Some(TGAImage::both_right as fn(&usize, &usize, &usize) -> (usize, usize)),
-            1 => clousure = Some(TGAImage::one_left_one_right as fn(&usize, &usize, &usize) -> (usize, usize)),
-            _ => {
-                clousure = Some(TGAImage::both_left as fn(&usize, &usize, &usize) -> (usize, usize));
-                let swap = tg_right;
-                tg_right = tg_left;
-                tg_left = swap;
+        y_axis_middle=Point::new(last_dot.x,middle_dot.y);
+        tg_migle = line_length(&y_axis_middle, middle_dot) / line_length(last_dot, &y_axis_middle);
+        if tg_migle.is_infinite(){tg_migle=0.0}
+        if middle_dot.x>last_dot.x{tg_migle=tg_migle*(-1.0)}
+        for dy in middle_dot.y+1..last_dot.y{
+            on_mid_border = on_mid_border + tg_migle;
+            on_last_border = on_last_border + tg_last;
+            let (start, end) = if on_mid_border > on_last_border { (on_last_border as usize, on_mid_border as usize) } else { (on_mid_border  as usize, on_last_border as usize) };
+            for x_coord in start..end+1{
+                self.set_pixel_unchecked(x_coord, dy, &color);
             }
         }
 
-        let mut midle_point=None;
-        for coord in final_coord.iter(){
-            if coord!=apex_point && coord!=bottom_point.unwrap(){
-                midle_point=Some(coord.clone());
-                break
-            }
-        };
-        let midle_point=midle_point.unwrap();
 
-        //flip direction if two apexes
-        let (dy_min,dy_max)=if two_min_y{
-            (0,apex_point.y + 1 - bottom_dot  )
-        }else {
-            (0 ,midle_point.y + 1 - apex_point.y)
-        };
-        for dy in dy_min..dy_max {
-            let right = (dy as f64 * tg_right) as usize;
-            let left = (dy as f64 * tg_left) as usize;
-            let (left,right)=if two_min_y{
-                (right,left)
-            }else {
-                (left,right)
-            };
-            let (start, length) = clousure.unwrap()(&left, &right, &apex_point.x);
-            let y_coord=if two_min_y{
-                apex_point.y-dy
-            }else{
-                dy + apex_point.y
-            };
-            for x_coord in start..start + length {
-                self.set_pixel_unchecked(x_coord, y_coord, &color);
-            }
-        }
-        if !two_min_y {
-            let right = (dy_max as f64 * tg_right) as usize;
-            let left = (dy_max as f64 * tg_left) as usize;
-            let (start,length)=clousure.unwrap()(&left,&right,&apex_point.x);
-            let new_point =match counter {
-                0=>Point::new(start+length,midle_point.y),
-                1=>{
-                    let point=Point::new(start+length,midle_point.y);
-                    if  point==midle_point{ Point::new(start,midle_point.y)}
-                    else{point} },
-                _=>Point::new(start,midle_point.y)
-            };
-            let mut new_point=Point::new(start+length,midle_point.y);
-            if  new_point==midle_point{
-                new_point=Point::new(start,midle_point.y);
-            }
-            let bottom_point=bottom_point.unwrap().clone();
-            let final_coord=[midle_point,bottom_point,new_point];
-            println!("before recursive {:?}",&final_coord);
-            self.fill_triangle(color,&final_coord);
-        }
     }
 
     fn both_right(left:&usize,right:&usize,apex_x:&usize)->(usize,usize){
--- src/main.rs
+++ src/main.rs
@@ -11,40 +11,72 @@ use simpleOpenGL::TGAImage;
 
 
 const FILE_PATH:&str="image.tga";
-
+const LIGHT_DIR:Vector=Vector(0.0,0.0,1.0);
 fn main() {
     let WHITE:TGAColor=TGAColor::new(255,255,255,255);
     let RED:TGAColor=TGAColor::new(255,0,0,255);
     let GREEN:TGAColor=TGAColor::new(0,255,0,255);
 
 
-    let mut tga_image=TGAImage::new(2000,2000);
+    let mut tga_image=TGAImage::new(10000,10000);
 
 
     let (points,triangles)=read_file();
     for triangle in triangles{
-        let mut triangle_real_coord:[(f32,f32);3]=[(0.0,0.0);3];
+        let mut triangle_real_coord:[(f32,f32,f32);3]=[(0.0,0.0,0.0);3];
         for (index,point) in triangle.iter().enumerate(){
             let point:[f32;3]=*points.get(*point-1).unwrap();
-            triangle_real_coord[index]=((point[0]+1.0)/2.0,(point[1]+1.0)/2.0);
+            triangle_real_coord[index]=((point[0]+1.0)/2.0,(point[1]+1.0)/2.0,point[2]);
         }
         let mut final_coord=Vec::new();
         for  (index,real_coorod) in triangle_real_coord.iter().enumerate(){
-            final_coord.push(Point::new((real_coorod.0*tga_image.width as f32)as usize,(real_coorod.1*tga_image.height as f32)as usize));
+            final_coord.push(D_point::new(real_coorod.0,
+            real_coorod.1,real_coorod.2));
         }
-        tga_image.fill_triangle(&GREEN,final_coord.as_slice());
+        let vec0=final_coord[0].to_vector(&final_coord[1]);
+        let vec1=final_coord[0].to_vector(&final_coord[2]);
 
-        tga_image.draw_line(&final_coord[0], &final_coord[1], &RED);
-        tga_image.draw_line(&final_coord[1], &final_coord[2], &RED);
-        tga_image.draw_line(&final_coord[2], &final_coord[0], &RED);
+        let triangle_normal=vec0.vector_prod(&vec1)
+            .normalize();
+        println!("normal {:?}",triangle_normal);
+        let intensity=triangle_normal.scalar_prod(&LIGHT_DIR);
+        println!("intensity {}",intensity);
+        if intensity>0.0{
+            let color=TGAColor::new((255.0*intensity) as u8,(255.0*intensity) as u8,(255.0*intensity) as u8,255);
+            tga_image.fill_triangle(&color,final_coord
+                .iter()
+                .map(|element|element.to_point(10000,10000))
+                .collect::<Vec<Point>>()
+                .as_slice());
+        }
 
-    }
-//    let final_coord=[Point::new(50,50),Point::new(1900,150),Point::new(1000,1900)];
-//    tga_image.fill_triangle(&GREEN,&final_coord);
-//
+
 //        tga_image.draw_line(&final_coord[0], &final_coord[1], &RED);
 //        tga_image.draw_line(&final_coord[1], &final_coord[2], &RED);
 //        tga_image.draw_line(&final_coord[2], &final_coord[0], &RED);
+
+    }
+
+//    let t0=[Point::new(10,70),Point::new(50,160),Point::new(70,80)];
+//    let t1=[Point::new(180,50),Point::new(150,1),Point::new(70,180)];
+//    let t2=[Point::new(180,150),Point::new(120,160),Point::new(130,180)];
+//
+//    tga_image.fill_triangle(&GREEN,&t0);
+//    tga_image.fill_triangle(&GREEN,&t1);
+//    tga_image.fill_triangle(&GREEN,&t2);
+//
+//
+//        tga_image.draw_line(&t0[0], &t0[1], &RED);
+//        tga_image.draw_line(&t0[1], &t0[2], &RED);
+//        tga_image.draw_line(&t0[2], &t0[0], &RED);
+//
+//    tga_image.draw_line(&t1[0], &t1[1], &RED);
+//    tga_image.draw_line(&t1[1], &t1[2], &RED);
+//    tga_image.draw_line(&t1[2], &t1[0], &RED);
+//
+//    tga_image.draw_line(&t2[0], &t2[1], &RED);
+//    tga_image.draw_line(&t2[1], &t2[2], &RED);
+//    tga_image.draw_line(&t2[2], &t2[0], &RED);
 
     tga_image.write_tga_file(FILE_PATH);
 
@@ -84,4 +116,43 @@ fn read_file()->(Vec<[f32;3]>,Vec<[usize;3]>)
     }
     (points,triangles)
 }
+
 struct Triangle((usize,usize),(usize,usize),(usize,usize));
+#[derive(Debug)]
+struct Vector(f32,f32,f32);
+impl Vector{
+    fn vector_prod(&self,vector:&Vector)->Vector{
+        Vector(self.1*vector.2-self.2*vector.1,
+               self.2*vector.0-self.0*vector.2,
+               self.0*vector.1-self.1*vector.0)
+    }
+    fn normalize(mut self)->Vector{
+        let inv_length=1.0/self.length();
+        self.0=self.0*inv_length;
+        self.1=self.1*inv_length;
+        self.2=self.2*inv_length;
+        self
+    }
+    fn scalar_prod(&self,vector:&Vector)->f32{
+        self.0*vector.0+self.1*vector.1+self.2*vector.2
+    }
+    fn length(&self)->f32{
+        (self.0*self.0+self.1*self.1+self.2*self.2).sqrt()
+    }
+}
+
+struct D_point{
+    x:f32,
+    y:f32,
+    z:f32}
+impl D_point{
+    fn new(x:f32,y:f32,z:f32)->D_point{
+        D_point{x,y,z}
+    }
+    fn to_point(&self,height:usize,width:usize)->Point{
+        Point::new((self.x*width as f32 )as usize,(self.y*height as f32) as usize)
+    }
+    fn to_vector(&self,end_of_vector:&D_point)->Vector{
+        Vector(end_of_vector.x-self.x,end_of_vector.y-self.y,end_of_vector.z-self.z)
+    }
+}
